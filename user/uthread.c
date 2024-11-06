0c90541ec0d6f80e7dee07f3f9fb7e3adb1b6d9c
kurtlin <ardt4113c@gmail.com>
2024-11-07 01:26:08 +0800

    [LAB6-1] Preserve call-saved regs, sp, ra in memory
    when doing thread_switch, swap preserved context into registers

--- user/uthread.c
+++ user/uthread.c
@@ -1,5 +1,5 @@
-#include "kernel/stat.h"
 #include "kernel/types.h"
+#include "kernel/stat.h"
 #include "user/user.h"
 
 /* Possible states of a thread: */
@@ -10,9 +10,28 @@
 #define STACK_SIZE 8192
 #define MAX_THREAD 4
 
+struct thread_context {
+    uint64 ra;
+    uint64 sp;
+
+    uint64 s0;
+    uint64 s1;
+    uint64 s2;
+    uint64 s3;
+    uint64 s4;
+    uint64 s5;
+    uint64 s6;
+    uint64 s7;
+    uint64 s8;
+    uint64 s9;
+    uint64 s10;
+    uint64 s11;
+};
+
 struct thread {
     char stack[STACK_SIZE]; /* the thread's stack */
     int state;              /* FREE, RUNNING, RUNNABLE */
+    struct thread_context context;
 };
 struct thread all_thread[MAX_THREAD];
 struct thread *current_thread;
