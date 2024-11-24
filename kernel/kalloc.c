61fdcf3e147fac6ef4de6d3d58be4ca902d220a9
lylin1997 <lylin1997.cs09g@nctu.edu.tw>
2024-11-25 01:03:07 +0800

    [LAB8-1] Done
    Allocate and freelist for each cpu

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -21,12 +21,17 @@ struct run {
 struct {
   struct spinlock lock;
   struct run *freelist;
-} kmem;
+} kmem[NCPU];
 
 void
 kinit()
 {
-  initlock(&kmem.lock, "kmem");
+  char lock_name[20];
+  for (int i = 0; i < NCPU; i++) {
+    snprintf(lock_name, sizeof(lock_name), "kmem_%d", i);
+    initlock(&kmem[i].lock, lock_name);
+  }
+
   freerange(end, (void*)PHYSTOP);
 }
 
@@ -56,10 +61,16 @@ kfree(void *pa)
 
   r = (struct run*)pa;
 
-  acquire(&kmem.lock);
-  r->next = kmem.freelist;
-  kmem.freelist = r;
-  release(&kmem.lock);
+
+  push_off();
+  const int cpu_id = cpuid(); // must turn off interrupts to be able to call cpuid()
+
+  acquire(&kmem[cpu_id].lock);
+  r->next = kmem[cpu_id].freelist;
+  kmem[cpu_id].freelist = r;
+  release(&kmem[cpu_id].lock);
+
+  pop_off();
 }
 
 // Allocate one 4096-byte page of physical memory.
@@ -70,13 +81,37 @@ kalloc(void)
 {
   struct run *r;
 
-  acquire(&kmem.lock);
-  r = kmem.freelist;
-  if(r)
-    kmem.freelist = r->next;
-  release(&kmem.lock);
+  push_off();
+  const int cpu_id = cpuid(); // must turn off interrupts to be able to call cpuid()
 
-  if(r)
+  acquire(&kmem[cpu_id].lock);
+  r = kmem[cpu_id].freelist;
+  if(r) {
+    kmem[cpu_id].freelist = r->next;
+  } else {
+    // no free pages on current cpu, try to steal 1 from another cpu
+    for (int i = 0; i < NCPU; i++) {
+      if (i == cpu_id) {
+        continue;
+      }
+
+      acquire(&kmem[i].lock);
+      r = kmem[i].freelist;
+      if (r) {
+        kmem[i].freelist = r->next;
+        release(&kmem[i].lock);
+        break;
+      }
+      release(&kmem[i].lock);
+    }
+  }
+
+  release(&kmem[cpu_id].lock);
+  pop_off();
+
+  if(r) {
     memset((char*)r, 5, PGSIZE); // fill with junk
+  }
+
   return (void*)r;
 }
